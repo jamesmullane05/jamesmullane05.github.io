b316290771602053bacba42c8fc56d34cd9dda37
James Mullane <jamesmullane05@gmail.com>
2026-03-17 04:04:38 +1300

    Adding Github Token

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -9,21 +9,39 @@ export class GithubService {
     }
 
     async getProfile(): Promise<GithubProfile>{
-       const data: Response = await fetch(`https://api.github.com/repos/${this.username}`)
+       const data: Response = await fetch(`https://api.github.com/repos/${this.username}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+                Accept: "application/vnd.github+json",
+            },
+            cache: "no-store"
+        })
        if (!data.ok) { throw new Error("Failed to fetch repositories")}
        const profile: GithubProfile = await data.json()
        return profile
     }
 
     async getProjects(): Promise<GithubRepo[]>{
-        const data: Response = await fetch(`https://api.github.com/users/${this.username}/repos`)
+        const data: Response = await fetch(`https://api.github.com/users/${this.username}/repos`, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+                Accept: "application/vnd.github+json",
+            },
+            cache: "no-store"
+        })
         if (!data.ok) { throw new Error("Failed to fetch repositories")}
         const projects: GithubRepo[] = await data.json()
         return projects
     }
 
     async getRepoData(project:string): Promise<GithubRepo>{
-        const data: Response = await fetch(`https://api.github.com/repos/${this.username}/${project}`);
+        const data: Response = await fetch(`https://api.github.com/repos/${this.username}/${project}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+                Accept: "application/vnd.github+json",
+            },
+            cache: "no-store"
+        });
         if (!data.ok) { throw new Error("Failed to fetch repositories")}
         const repo: GithubRepo = await data.json();
         return repo
